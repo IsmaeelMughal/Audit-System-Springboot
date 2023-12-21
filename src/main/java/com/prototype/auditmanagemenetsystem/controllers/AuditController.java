29d3d3b3df854a86d533b7cf30a4c73efcffd21c
IsmaeelMughal <ismaeel.mughal.1912@gmail.com>
2023-12-21 22:30:10 +0500

    Audit Controller

--- src/main/java/com/prototype/auditmanagemenetsystem/controllers/AuditController.java
+++ src/main/java/com/prototype/auditmanagemenetsystem/controllers/AuditController.java
@@ -1,10 +1,25 @@
 package com.prototype.auditmanagemenetsystem.controllers;
 
+import com.prototype.auditmanagemenetsystem.dtos.AuditDTO;
+import com.prototype.auditmanagemenetsystem.dtos.ResponseDTO;
+import com.prototype.auditmanagemenetsystem.services.AuditService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/audits")
 public class AuditController {
+
+    private final AuditService auditService;
+
+    @GetMapping
+    public ResponseDTO<List<AuditDTO>> getAllAudits(){
+        return auditService.getAllAudits();
+    }
 
 }
