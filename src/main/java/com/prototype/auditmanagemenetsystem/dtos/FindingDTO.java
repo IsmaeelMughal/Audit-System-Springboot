4578a97d13ec292aaa6e205dd053d3b3f70790e5
IsmaeelMughal <ismaeel.mughal.1912@gmail.com>
2023-12-21 22:19:51 +0500

    Finding Id

--- src/main/java/com/prototype/auditmanagemenetsystem/dtos/FindingDTO.java
+++ src/main/java/com/prototype/auditmanagemenetsystem/dtos/FindingDTO.java
@@ -22,5 +22,5 @@ public class FindingDTO {
     private String recommendation;
     private String responsibleDepartment;
     private String timeline;
-    private Integer audit;
+    private Integer auditId;
 }
